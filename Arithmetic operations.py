43dfcb711e033ce52c2dfef45d15d95d4d9850d6
1intekhab7 <ia5931955@gmail.com>
2024-07-11 17:38:57 +0530

    Create Arithmetic operations.py

---
+++ Arithmetic operations.py
@@ -0,0 +1,12 @@
+# simple python programe to understand the
+arithmetic operations multiplication
+#here, we storing the first input mumber in num1
+num1=  input("Enter first number:")
+#here, we storing the second input number in num2
+num2= input("Enter second number:')
+#here, we declaring a variable mul to store the result
+#here we are using arithmetical operations to
+multiply the two numbers 
+Mul= float(num1)*float(num2)
+#here, we printing the multiplicatin of the given two numbers
+ 
